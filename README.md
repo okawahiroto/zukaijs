c4bb3efb2b8c12b9881fe0ac9b0132b1ef638781
okawahiroto <okawa.hiroto@gmail.com>
2021-08-15 22:20:15 +0900

    インクリメント

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # zukaijs
+
+変数は　let
+定数は　const
--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 const price = 150;
 let input = 200;
 console.log(input - price);
+
+let answer = false;
+if (answer) {
+  console.log("正解です");
+} else {
+  console.log("間違いです");
+};
+
+console.log(10 + 11);
+console.log(10 - 11);
+console.log(10 * 11);
+console.log(10 / 2);
+console.log(10 % 7);
+
+let count = 1;
+count++;
+console.log(count);
+
+let count_2 = 1;
+count_2 = count_2 + 1;
+console.log(count_2);
+
+let count_d = 1;
+count_d--;
+console.log(count_d);
